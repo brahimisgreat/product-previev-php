a1ac8f586229649e5de73d8bf3704fc1e34055d7
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2025-03-24 00:56:15 -0400

    sfdf

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <main>
+        <section>
+            <img src="./assets/image-product-mobile.jpg" alt="">
+            <div class="product">
+                <h4>PERFUME</h4>
+                <h2>Gabrielle Essence Eau De Parfum</h2>
+                <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+                <div class="price">
+                    <span>$149.99</span>
+                    <p><s>169.99</s></p>
+                </div>
+                <button> <img src="./assets/icon-cart.svg" alt=""> ADD TO CART</button>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
