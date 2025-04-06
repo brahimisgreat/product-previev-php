7d426a0c9642dd91f59f3e6272d94132d4cf4459
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2025-04-05 21:01:54 -0400

    sfd

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ const count = document.getElementById("count");
 const minusButton = document.getElementById("minusButton");
 const plusButton = document.getElementById("plusButton");
 
+const cartcount= document.getElementById("count")
+
 
 let cart = 0
-
-
+cartcount.innerHTML = cart
 
 minusButton.addEventListener("click", doSomething)
+plusButton.addEventListener("click", doSomethingElse)
 
-button.addEventListener("click", doSomething);
+
 function doSomething(){
+    cart -= 1
+    console.log(cart)
+    cartcount.innerHTML = cart
+}
+function doSomethingElse(){
     cart += 1
     console.log(cart)
-    alert("it worked");
+    cartcount.innerHTML = cart
 }
 
 
