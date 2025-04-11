7f7b4cd78b58f9ac46f8d2909510494d2302812a
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2025-04-10 22:34:17 -0400

    sfdfd

--- index copy.php
+++ index copy.php
@@ -10,14 +10,36 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <script src="./script.js" defer></script>
-</head> 
+</head>
 
 <body>
     <main>
-        
+        <form action="">
+            <label for="firstname">Firstname?</label>
+            <input type="text" id="name" placehold>
+
+            <label for="lastname">Lastname?</label>
+            <input type="text" id="name" name="lastname" placehold>
+
+            <label for="email">Favourite Pet</label>
+            <select name="favouritepet" id="favoritepet">
+                <option value="none">None</option>
+                <option value="dog">Dog</option>
+                <option value="Cat">Cat</option>
+                <option value="Bird">Bird</option>
+            </select>
+
+            <button>Submit</button>
+            
+
+
+            
+
+            
+
+        </form>
     </main>
 
-   
 </body>
 
 </html>
